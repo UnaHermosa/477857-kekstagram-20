1ccf549b6085b902b6896511dd0c3e18ed4e170c
Елена <veintitress@yandex.ru>
2020-07-12 08:59:08 +0300

    Правки

--- js/custom-image.js
+++ js/custom-image.js
@@ -2,13 +2,13 @@
 (function () {
   var uploadFile = document.querySelector('.img-upload__input');
   var imgPreview = document.querySelector('.img-upload__preview img');
-  var FILE_TYPES = ['jpg', 'jpeg', 'png'];
+  var fileTypes = ['jpg', 'jpeg', 'png'];
 
   uploadFile.addEventListener('change', function () {
     window.main.openEditingModal();
     var file = uploadFile.files[0];
     var fileName = file.name.toLowerCase();
-    var matches = FILE_TYPES.some(function (item) {
+    var matches = fileTypes.some(function (item) {
       return fileName.endsWith(item);
     });
 
